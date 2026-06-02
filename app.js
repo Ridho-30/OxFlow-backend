1ae83bdf3033960d26abe1ff5d71b53a7426cd04
Ridho-30 <ridhorizkyprasetyo30@gmail.com>
2026-06-02 10:07:58 +0700

    add vercel config and swagger Fix

--- app.js
+++ app.js
@@ -83,21 +83,21 @@ app.use((req, res) => {
 });
 
 // Start Server
-const server = app.listen(env.PORT, async () => {
-  console.log(`🚀 OxFlow API running on http://localhost:${env.PORT}`);
-  console.log(`📚 Swagger docs: http://localhost:${env.PORT}/api-docs`);
+// const server = app.listen(env.PORT, async () => {
+//   console.log(`🚀 OxFlow API running on http://localhost:${env.PORT}`);
+//   console.log(`📚 Swagger docs: http://localhost:${env.PORT}/api-docs`);
   
-  // // Test Database Connection
-  // try {
-  //   await prisma.$connect();
-  //   console.log('🔌 Database connection verified');
-  // } catch (err) {
-  //   console.error('❌ DATABASE ERROR FATAL:', err);
-  //   console.log('Menutup server karena database tidak siap...');
-  //   server.close(() => {
-  //     process.exit(1);
-  //   });
-  // }
-});
+//   // // Test Database Connection
+//   // try {
+//   //   await prisma.$connect();
+//   //   console.log('🔌 Database connection verified');
+//   // } catch (err) {
+//   //   console.error('❌ DATABASE ERROR FATAL:', err);
+//   //   console.log('Menutup server karena database tidak siap...');
+//   //   server.close(() => {
+//   //     process.exit(1);
+//   //   });
+//   // }
+// });
 
 module.exports = app;
